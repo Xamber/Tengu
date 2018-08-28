67756683a716af0471a5d32c7a51be9d3e940806
Artem Filippov <Artem.Filippov@emc.com>
2018-08-28 16:10:48 +0300

    Refactor

--- src/lib/viewer.js
+++ src/lib/viewer.js
@@ -2,47 +2,52 @@ import React, { Component } from 'react';
 import Card from './card';
 import {shuffleArray} from './utils';
 
-const TIMEOUT = 1300;
+const TIMEOUT = 1500;
 
 export default class Viewer extends Component {
     constructor(props) {
         super(props)
-        this.ref = React.createRef();
-        this.dict = props.dict;
-        this.state = {
-            "rus": ["Привет!"],
-            "eng": "Hello!",
-            "usage": "Fast English words",
-        }
-
+        
         this.pickNew = this.pickNew.bind(this);
         this.handleTouch = this.handleTouch.bind(this);
         this.handleTouchEnd = this.handleTouchEnd.bind(this);
 
-        this._keys = Object.keys(this.dict)
-        shuffleArray(this.dict);
+        this.state = {
+            "rus": ["Привет!"],
+            "eng": "Hello!",
+            "usage": "Fast English words",
+            "next": 0,
+            "ticker": setInterval(this.pickNew, TIMEOUT),
+        }
 
-        this.next = 0;
-        this.ticker = setInterval(this.pickNew, TIMEOUT)
+        this.ref = React.createRef();
+
+        this.dict = props.dict;
+        this._keys = Object.keys(this.dict)
+        
+        shuffleArray(this._keys);
     }
 
     pickNew() {
-        let word = this.dict[this._keys[this.next]]
+        let word = this.dict[this._keys[this.state.next]]
         this.setState({
             "rus": word.rus,
             "eng":  word.eng,
             "usage": word.usage ? word.usage[0] : "",
+            "next": this.state.next + 1
         });
-        this.next++;
     }
 
     handleTouch() {
-        clearInterval(this.ticker);
+        clearInterval(this.state.ticker);
     }
 
     handleTouchEnd() {
         this.pickNew()
-        this.ticker = setInterval(this.pickNew, TIMEOUT)
+        let ticker = setInterval(this.pickNew, TIMEOUT)
+        this.setState({
+            "ticker": ticker
+        })
     }
 
     componentDidMount() {
