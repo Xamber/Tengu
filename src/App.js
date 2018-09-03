40618107cc9e5517bfe51e017a0ca5ace01c3d75
Artem Filippov <Artem.Filippov@emc.com>
2018-09-03 10:31:33 +0300

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import Viewer from './lib/viewer'
+import Viewer from './components/viewer'
 import Dictionary from './data/english_words.json';
 
-const shuffleArray = function (arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
+const initialize = function (dict) {
+
+  let keys = Object.keys(dict);
+  keys = keys.filter(word => dict[word].usage.length > 0)
+
+  for (let i = keys.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
+      [keys[i], keys[j]] = [keys[j], keys[i]];
   }
+
+  return {dict, keys}
 }
 
-let keys = Object.keys(Dictionary);
-keys = keys.filter(word => Dictionary[word].usage.length > 0)
-shuffleArray(keys);
+const {dict, keys} = initialize(Dictionary)
 
 class App extends Component {
   render() {
+
+    console.log(keys, dict)
     return (
-      <Viewer keys={keys} dict={Dictionary}></Viewer>
+      <Viewer keys={keys} dict={dict}></Viewer>
     );
   }
 }
